b95d9ccda9c1300863aa7d6500ae5e2f67a97be9
McCray <tubach75@hotmail.com>
2021-08-31 12:39:14 -0400

    commit tree f2f4ff5bfc697f086f576a5c4b07de8c13afadd4

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {actors.map((actor, idx) => (
+            <div key={idx}>
+                <p> Actor Name: {actor.name}</p>
+                <p> Actor Movies:</p>
+                <ul>
+                    {actor.movies.map((movie, index) => (
+                        <li key={index}>{movie}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
     </div>
   );
 };
